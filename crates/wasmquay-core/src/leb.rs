6d88af6a6d53a6a6d7e42761b79e5bd057374023
britsman <670985+britsman@users.noreply.github.com>
2018-04-12 15:05:00 +0700

    chore: bump ci workflow

---
+++ crates/wasmquay-core/src/leb.rs
@@ -0,0 +1,19 @@
+//! A cursor over a byte slice with the primitive decoders the WebAssembly
+//! binary format needs: fixed-width little-endian integers, unsigned/signed
+//! LEB128 variable-length integers, and length-prefixed UTF-8 names.
+//!
+//! All methods are bounds-checked and return a [`Result`]; they never panic on
+//! malformed input.
+
+use crate::error::{Error, ErrorKind, Result};
+
+/// A forward-only reader over a byte slice.
+#[derive(Debug, Clone)]
+pub struct Reader<'a> {
+    data: &'a [u8],
+    pos: usize,
+}
+
+impl<'a> Reader<'a> {
+    /// Create a reader over `data`, positioned at offset 0.
+    pub fn new(data: &'a [u8]) -> Self {
