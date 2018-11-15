f5c1bffd90de096b176570fdb7915f165afc3c00
britsman <670985+britsman@users.noreply.github.com>
2018-11-15 10:34:00 +0700

    docs: expand policy enforcement

--- crates/wasmquay-core/src/error.rs
+++ crates/wasmquay-core/src/error.rs
@@ -8,3 +8,12 @@ use std::fmt;
 
 /// The category of a failure. Useful for programmatic handling and for
 /// choosing an exit code in the CLI.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ErrorKind {
+    /// The byte stream ended before a structure was fully decoded.
+    UnexpectedEof,
+    /// A magic number, version or section id was not what the spec requires.
+    BadMagic,
+    /// A LEB128 integer was malformed (too long / overflowing).
+    BadLeb128,
+    /// A UTF-8 name inside the module was not valid UTF-8.
