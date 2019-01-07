c4ca61b39d2aa3c654bfa2fd0d7230075a4cad5d
britsman <670985+britsman@users.noreply.github.com>
2019-01-07 15:39:00 +0700

    docs: document zero dependencies

--- crates/wasmquay-core/src/lib.rs
+++ crates/wasmquay-core/src/lib.rs
@@ -8,3 +8,11 @@
 //! 1. [`leb`] — primitive byte / LEB128 decoding.
 //! 2. [`wasm`] — the WebAssembly binary decoder (header, sections, imports,
 //!    exports, custom `name` section).
+//! 3. [`wit`] — a reader for wasmquay's WIT-like interface manifest subset.
+//! 4. [`policy`] — capability classification and explicit policy evaluation.
+//! 5. [`compat`] — substitutability comparison between two components.
+//! 6. [`report`] — stable JSON/text serialization consumed downstream.
+//! 7. [`fixture`] — a matching binary *encoder* for deterministic test data.
+//!
+//! ## Scope & honesty
+//!
