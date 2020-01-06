12186efe4ad83c0d79ff9a04d8d26ab32a9da916
britsman <670985+britsman@users.noreply.github.com>
2020-01-06 13:21:00 +0700

    style: clean up zero dependencies

--- explorer/src/index.ts
+++ explorer/src/index.ts
@@ -2,3 +2,5 @@
  * wasmquay-explorer — public API.
  *
  * A static, dependency-free TypeScript library that consumes the JSON reports
+ * produced by the `wasmquay` Rust CLI and turns them into capability surfaces,
+ * risk scores and policy reconciliation findings.
