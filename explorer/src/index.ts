84310d1c1107d2b9c537040cbd477faf802c8966
britsman <670985+britsman@users.noreply.github.com>
2020-01-06 13:33:00 +0700

    chore: bump leb128 reader

--- explorer/src/index.ts
+++ explorer/src/index.ts
@@ -4,3 +4,6 @@
  * A static, dependency-free TypeScript library that consumes the JSON reports
  * produced by the `wasmquay` Rust CLI and turns them into capability surfaces,
  * risk scores and policy reconciliation findings.
+ *
+ * It performs no WebAssembly execution and no I/O of its own (the CLI wrapper
+ * in `cli.ts` handles file reading); every export here is a pure function of
