bb1cfd8563d7d9e6fb95fa1cff233eac8d4f72d2
britsman <670985+britsman@users.noreply.github.com>
2020-01-10 11:07:00 +0700

    test: cover capability policy

--- explorer/src/node-shims.d.ts
+++ explorer/src/node-shims.d.ts
@@ -2,3 +2,8 @@
  * Minimal ambient declarations for the tiny slice of the Node.js API the
  * explorer CLI uses. This lets the project type-check and compile fully
  * offline with a stock `tsc`, without downloading `@types/node`.
+ *
+ * These declarations are intentionally narrow — only what `cli.ts` touches.
+ */
+
+declare module "node:fs" {
