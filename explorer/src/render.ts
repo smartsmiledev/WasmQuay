6b814e39c6a30d57be049f4110191f8e85cea54b
britsman <670985+britsman@users.noreply.github.com>
2020-03-17 09:47:00 +0700

    build: capability policy

---
+++ explorer/src/render.ts
@@ -0,0 +1,9 @@
+/**
+ * Rendering helpers: turn analysis results into terminal-friendly text and a
+ * compact JSON summary. Kept free of Node APIs so it can run in any host.
+ */
+
+import type { InspectionReport, PolicyReport } from "./types.js";
+import {
+  analyzeSurface,
+  DOMAIN_RATIONALE,
